204fe3903b659095f9cb003d34c89da0a7fc0057
Frank Thelen <frank.thelen@telefonica.com>
2018-03-06 14:04:23 +0100

    prepare for multiple paths / 2

--- src/phrasal.js
+++ src/phrasal.js
@@ -3,11 +3,13 @@ const evaluate = (options, config, target, prop) => {
   const { key, values: val = [key] } = path[0]; // next
   const values = typeof val === 'function' ? val.bind(bind)(options) : val;
   if (values.includes(prop)) {
+    const remaining = path.slice(1);
     return {
       fn,
       bind,
-      path: path.slice(1), // remaining
       options: { ...options, [key]: prop },
+      config: { fn, bind, path: remaining },
+      further: remaining.length > 0,
     };
   }
   return false; // no match
@@ -20,11 +22,10 @@ const phrasal = (options, config) => ({ // TODO: support multiple paths
     }
     const next = evaluate(options, config, target, prop);
     if (next) {
-      const { fn, bind, path: newPath, options: newOptions } = next;
-      if (newPath.length) {
-        return new Proxy(target, phrasal(newOptions, { fn, bind, path: newPath }));
-      }
-      return (...args) => fn.bind(bind)(newOptions, ...args);
+      const { fn, bind, options: newOptions, config: newConfig, further } = next;
+      return further
+        ? new Proxy(target, phrasal(newOptions, newConfig))
+        : (...args) => fn.bind(bind)(newOptions, ...args);
     }
     if (typeof prop === 'string') {
       throw new Error(`unknown term in phrasal function: ${prop}`);
